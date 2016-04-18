d0c705bbfc46bb49cc2408b63592c3e26afaaead
icchiy <icchi050502@gmail.com>
2016-04-18 09:59:08 +0900

    commited to login after git reset.

--- Gemfile
+++ Gemfile
@@ -50,4 +50,3 @@ end
   gem 'haml-rails'
   gem 'erb2haml'
   gem 'bootstrap-sass', '~> 3.3.6'
-  gem 'devise'
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,5 @@
 class PrototypesController < ApplicationController
 
-  # before_action :move_to_index, except: :index
-
   def index
   end
 
@@ -10,9 +8,5 @@ class PrototypesController < ApplicationController
 
   def show
   end
-
-  # def move_to_index
-  #   redirect_to action: :index unless user_signed_in?
-  # end
 
 end
