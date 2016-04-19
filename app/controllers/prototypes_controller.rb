6b0b5bed40883129f38b7a7a530b3949b7603bbb
icchiy <icchi050502@gmail.com>
2016-04-19 23:41:23 +0900

    modification of the source code has been completed.reatart from login function.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
   end
 
@@ -8,5 +10,11 @@ class PrototypesController < ApplicationController
 
   def show
   end
+
+   def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+
 
 end
